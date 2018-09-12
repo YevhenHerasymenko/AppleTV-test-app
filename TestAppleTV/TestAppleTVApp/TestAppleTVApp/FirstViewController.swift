3b7d0aa9ce3eb13b67ab4dce2935ec26eb256306
Yevhen Herasymenko <yhe@ciklum.com>
2018-09-12 16:08:44 +0300

    demo app

--- TestAppleTV/TestAppleTVApp/TestAppleTVApp/FirstViewController.swift
+++ TestAppleTV/TestAppleTVApp/TestAppleTVApp/FirstViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+  @IBOutlet weak var button1: UIButton!
+  @IBOutlet weak var button2: UIButton!
+  @IBOutlet weak var button3: UIButton!
+  @IBOutlet weak var button4: UIButton!
+  @IBOutlet weak var button5: UIButton!
+  @IBOutlet weak var button6: UIButton!
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    let focusGuide = UIFocusGuide()
+
+    view.addLayoutGuide(focusGuide)
+
+    focusGuide.widthAnchor.constraint(equalTo: button1.widthAnchor).isActive = true
+    focusGuide.leftAnchor.constraint(equalTo: button1.leftAnchor).isActive = true
+    focusGuide.topAnchor.constraint(equalTo: button1.bottomAnchor).isActive = true
+    focusGuide.bottomAnchor.constraint(equalTo: button2.topAnchor).isActive = true
+
+    focusGuide.preferredFocusEnvironments = [button6]
+  }
+
+  override var preferredFocusEnvironments: [UIFocusEnvironment] {
+    return [button3]
+  }
+
+  @IBAction func action() {
+    setNeedsFocusUpdate()
+    updateFocusIfNeeded()
+  }
+
+  override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+    if context.nextFocusedView == button4 {
+      button1.setTitle("Ololo", for: .normal)
+    } else {
+      button1.setTitle("Button1", for: .normal)
+    }
   }
 }
 
--- TestAppleTV/TestAppleTVApp/TestAppleTVApp/SecondViewController.swift
+++ TestAppleTV/TestAppleTVApp/TestAppleTVApp/SecondViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UITableViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
   }
 
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+    cell.textLabel?.text = "Index \(indexPath.row)"
+
+    return cell
+  }
+
+  override func tableView(_ tableView: UITableView, canFocusRowAt indexPath: IndexPath) -> Bool {
+    return indexPath.row != 5
+  }
+
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    guard indexPath.row == 7 else {
+      return
+    }
+    setNeedsFocusUpdate()
+    updateFocusIfNeeded()
+  }
+
+  override func indexPathForPreferredFocusedView(in tableView: UITableView) -> IndexPath? {
+    return IndexPath(row: 2, section: 0)
+  }
 
 }
 
