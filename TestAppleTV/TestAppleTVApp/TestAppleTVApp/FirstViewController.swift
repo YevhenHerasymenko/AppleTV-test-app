f9f78d37aaddcaa3387207fbc45a774b272fa48a
Yevhen Herasymenko <yhe@ciklum.com>
2018-09-12 12:08:42 +0300

    add app

---
+++ TestAppleTV/TestAppleTVApp/TestAppleTVApp/FirstViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FirstViewController.swift
+//  TestAppleTVApp
+//
+//  Created by YevhenHerasymenko on 9/11/18.
+//  Copyright © 2018 Ciklum. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+}
+
